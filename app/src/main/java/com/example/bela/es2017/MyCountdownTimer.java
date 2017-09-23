b955c1853c9b0e07b37bb4f314f2ffa70a5bf858
brunogata <emaildoogata@gmail.com>
2017-09-23 17:37:00 -0300

    Criação do timer. Adendos:    - MyCountDownTimer é a classe Timer que atualiza a TextView do cronômetro e faz a ação no final do tempo;    - TimerActivity é a activity responsável por pegar o tempo desejado do usuário e dar play ao timer. Ao clicar no botão, a activity Timer2Activity é direcionada;    - Timer2Activity é a activity que mostra o cronômetro;    - O "beep" (som após o término da contagem) ainda não é final.

---
+++ app/src/main/java/com/example/bela/es2017/MyCountdownTimer.java
@@ -0,0 +1,53 @@
+package com.example.bela.es2017;
+
+import android.content.Context;
+import java.util.Calendar;
+
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Build;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by brunogata on 23/09/17.
+ */
+
+public class MyCountdownTimer extends CountDownTimer {
+
+    private Context context;
+    private long tempo;
+    private TextView tvAux;
+
+    public MyCountdownTimer (Context context, long tempo, long intervalo, TextView tv){
+        super(tempo, intervalo);
+        this.context = context;
+        this.tvAux = tv;
+    }
+
+    @Override
+    public void onTick(long millisUntilFinished) {
+        tempo = millisUntilFinished;
+        tvAux.setText(getCorrectTimer(true, millisUntilFinished) + ":" + getCorrectTimer(false, millisUntilFinished));
+    }
+
+    @Override
+    public void onFinish() {
+        tempo -= 1000;
+        tvAux.setText(getCorrectTimer(true, tempo) + ":" + getCorrectTimer(false, tempo));
+        ToneGenerator beep = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+        beep.startTone(ToneGenerator.TONE_CDMA_PIP, 300);
+    }
+
+    private String getCorrectTimer(boolean isMinute, long millisUntilFinished){
+        String aux;
+        Calendar cAux = Calendar.getInstance();
+        int constCalendar = isMinute ? Calendar.MINUTE : Calendar.SECOND;
+        cAux.setTimeInMillis(millisUntilFinished);
+
+        aux = cAux.get(constCalendar) < 10 ? "0" + cAux.get(constCalendar) : ""+ cAux.get(constCalendar);
+        return aux;
+    }
+}
